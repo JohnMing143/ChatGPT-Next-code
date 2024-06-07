075df7c5f2003866e978b1ea9d3a3b9abbf85997
JohnMing143 <126879422+JohnMing143@users.noreply.github.com>
2024-06-07 22:44:18 +0800

    Update cn.ts
    cute

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -376,9 +376,9 @@ const cn = {
     },
   },
   Store: {
-    DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗",
-    Error: "出错了，稍后重试吧",
+    DefaultTopic: "新的遇见",
+    BotHello: "赛博小铭竭诚为您服务",
+    Error: "诶？搞错了，一会再来试试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
       Topic:
